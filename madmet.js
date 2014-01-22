341eff543ef5a05fe595835bb3e689d6e1e4cc02
Karin <karin.j.alvarez@gmail.com>
2014-01-22 15:45:09 -0700

    commit

--- madmet.js
+++ madmet.js
@@ -4,17 +4,29 @@
 
 	var timeArrived = new Date();
 
-
-	$('#button').on('click', function(){
-		var now = new Date();
+ 	$('#buttontwo').on('click', function() {
+ 		var now = new Date();
 		var timeAmount = now-timeArrived; 
-		alert("It took you "+timeAmount/1000+" seconds to click on the button.");
+		alert("It took you "+timeAmount/1000+" seconds to request free chocolate!");
+ 	})
+
+
+	$('#buttonone').on('click', function(){
+		
+		var now = new Date();
+		var timeAmount = now-timeArrived;
 		var scrollAmount = $(window).scrollTop();
   		var documentHeight = $(document).height();
   		var scrollPercent = (scrollAmount/documentHeight) * 100; 
     	alert("You have viewed this percentage of the page: "+scrollPercent+"\n"+
-    		  "You have scrolled this distance through the page, in pixels: "+scrollAmount);
+    		  "You have scrolled this distance through the page, in pixels: "+scrollAmount); 
+		alert("You have been on this page for "+timeAmount/1000+" seconds.");
 	})
 
  });
+
+
+
+
+
 
